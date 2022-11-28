bf5a1f5ffd82504364d329bb76d9dfe5458d743d
calher99 <calher99@gmail.com>
2022-11-28 11:43:43 +0000

    create function to modify class of active drop down

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 
-console.log('Hello');
+const topOptions = document.querySelectorAll('.drop-down');
+const dropDown = document.querySelectorAll('.drop-down-container');
+const topOptionArray = Array.from(topOptions);
+const dropDownArray = Array.from(dropDown);
+
+topOptionArray.forEach((option) => {
+  option.addEventListener('click', addActive);
+});
+topOptionArray.forEach((option) => {
+  option.addEventListener('mouseover', addActive);
+});
+
+function addActive(eventObj) {
+  const idTarget = eventObj.target.dataset.option;
+  const div = document.querySelector(idTarget);
+  div.classList.add('active');
+}
+
+function removeActive() {}
